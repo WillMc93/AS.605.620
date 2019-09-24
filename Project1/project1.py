9a1b074af37003495c6953cbaf74bf494e71aa05
Will McElhenney <wmcelhe1@jhu.edu>
2019-09-24 14:19:09 -0500

    Transfer to Linux

--- Project1/project1.py
+++ Project1/project1.py
@@ -9,7 +9,7 @@ William McElhenney
 Generator function that reads in the data and
 converts in to a usable form (size, matrix A, matrix B)
 """
-def read(path='\\inputs\\classdata.dat')
+def read(path='C:\\Users\\whm0004\\Google Drive\\Algorithms\\git\\Project1\\inputs\classinput.dat'):
 	# get that data
 	data = None
 	with open(path) as file:
@@ -31,21 +31,25 @@ def read(path='\\inputs\\classdata.dat')
 		matrixB = []
 
 		# Break the data into proper chunks.
-		size = data[0]
+		size = int(data[0])
 		matrices = data[1: 2*size + 1]
 
 		matrixA = matrices[0: size]
 		matrixB = matrices[size: 2*size]
 
 		# break up the matrix data into matrices
-		for line in matrixA:
-			line = line.split(' ')
-			if len(line) != size:
-				raise IndexError("The data for matrixA is improperly formatted.")
-		for line in matrixB:
-			line = line.split(' ')
-			if len(line) != size:
-				raise IndexError("The data for matrixB is improperly formatted.")
+		matrixA = make_matrix(matrixA)
+		matrixB = make_matrix(matrixB)
+		
+		# make sure that the matrices are mostly square
+		# (we'll probably check again, but here's a good spot too)
+		if len(matrixA) != len(matrixA[0]) != size:
+			raise IndexError("The data for matrixA is improperly formatted!")
+		if len(matrixB) != len(matrixB[0]) != size:
+			raise IndexError("The data for matrixB is improperly formatted!")
+
+
+		print(matrixA, matrixB)
 
 		# remove isolated data
 		# It's a bold strategy, Cotton. Let's see if it pays off for him.
@@ -66,24 +70,71 @@ def init_matrix(m, n):
 	return [[0] * n for _ in range(m)]
 
 """
+Converts matrix strings into lists of values.
+"""
+def make_matrix(matrix):
+	for idx in range(len(matrix)):
+		line = matrix[idx]
+		line = line.strip()
+		line = line.split(' ')
+
+		matrix[idx] = line
+
+	return matrix
+
+"""
 Implements basic matrix multiplication, O(n^3)
 """
 def basic(size, matrixA, matrixB):
+	# check that matrixA and B are (mostly) square
+	assert (len(matrixA) == len(matrixA[0]) == size)
+	assert (len(matrixB) == len(matrixB[0]) == size)
+
 	# initialize output matrix
 	outp = init_matrix(size, size)
 
 	# iterate through rows of matrixA
 	for m in range(size):
-   		# iterate through columns of matrixB
-   		for n in range(size):
-       		# iterate through rows of matrixB
-       		for n2 in range(size):
-           		result[m][n] += matrixA[m][n2] * matrixB[n2][n]
+		# iterate through columns of matrixB
+		for n in range(size):
+			# iterate through rows of matrixB
+			for n2 in range(size):
+				result[m][n] += matrixA[m][n2] * matrixB[n2][n]
 
-    return outp
+	return outp
+
+
+"""
+Function for writing matrices to output (file)
+"""
+def write_matrix(matrix, file):
+	for row in matrix:
+		for col in row:
+			file.write(num + ' ')
+
+		file.write('\n')
 
 """
 Outputs the results
 """
-def write(matrixA, matrixB, basic_time, s_time, path='\\outputs\\test.out'):
-	with open(path, 'a+'):
+def write(matrixA, matrixB, product, times, path='\\outputs\\test.out'):
+	with open(path, 'a+') as file:
+		file.write("Inputs:\n")
+
+		write_matrix(matrixA, file)
+		write_matrix(matrixB, file)
+
+		file.write("Outputs:\n")
+
+		write_matrix(product, file)
+
+		file.write('\n\n')
+
+		file.write(f"Average completion time for basic multiplication: {times['basic']}")
+		file.write(f"Average completion time for strassen multiplication: {times['strassen']}")
+
+
+if __name__ == '__main__':
+	for size, matrixA, matrixB in read():
+		product = basic(size, matrixA, matrixB)
+		write(matrixA, matrixB, product, {'basic': 0, 'strassen': 0})
