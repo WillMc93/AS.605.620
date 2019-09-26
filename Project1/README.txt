71d123b013b0563741fd1fb5108a15b952cb7132
Will McElhenney <wmcelhe1@jhu.edu>
2019-09-25 20:14:10 -0500

    Time to finish the report

--- Project1/README.txt
+++ Project1/README.txt
@@ -1,4 +1,4 @@
-project1.py located in the root of the folder is the driver for this program
+project1.py located in this same folder is the driver for this program
 	(a copy can also be found in ./code).  
 It was written under Xubuntu 19.04 and interpreted using Python 3.7.3
 
@@ -9,7 +9,7 @@ In *nix systems with Python 3 installed, it can be executed
 
 
 For Windows, I'd highly recommend opening the project file from IDLE and
-	using the Run Customized command (Shift + F5) to enter the paths.
+	using the "Run Customized" command (Shift + F5) to enter the paths.
 
 NOTE: It does expect the output directory to exist. 
 	I didn't want to go poking around in the file system for this project.
---
+++ Project1/code/project1_copy.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+#!/usr/bin/env python3
+
+"""
+605.420 Project 1 - Matrix Multiplication
+
+@ author William McElhenney
+@ version 1.0 09/24/19	
+"""
+import sys
+
+# custom code
+sys.path.append('./code/') # make sure we can find the following from root
+from fileIO import read, write
+from matrix_tools import multiply
+from strassen import strassen
+
+"""
+MAIN
+
+Reads in the data, runs the multiplications, 
+and gets the results written
+"""
+if __name__ == '__main__':
+	# Get the command line arguments
+	infile = sys.argv[1]
+	outfile = sys.argv[2]
+
+	# Reinitialize output file if it already exists
+	with open(outfile, 'w+') as file:
+		file.write('')
+
+	# Do the things
+	try:
+		for size, matrixA, matrixB in read(infile):
+			productB = multiply(matrixA, matrixB)
+			print("Successfully multiplied!")
+			productS = strassen(matrixA, matrixB)
+			print("Successfully (Strassen) multiplied!")
+
+			assert(productB == productS) # sanity check
+
+			write(matrixA, matrixB, productB, productS, outfile)
+
+	# Need to tell the user so they know to check the file		
+	except FileNotFoundError as e:
+		print(str(e))
+
+	# Let the user know an error was encountered (should be on their end)
+	except Exception as e: 
+		print("Something went wrong. The input data is likely has a " + \
+			"formatting issue.")
+		print("Make sure the input only consists of " + \
+			"numbers, and that the size matches the matrix.")
+		print('Caught Exception:\n\t\t' + str(e) + "\n\n")
+		with open(outfile, 'a+') as file:
+			file.write(str(e))
