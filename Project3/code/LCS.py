be354ac40421bf9ce79b5ed0ca97a88aac77d1c4
Will McElhenney <whm0004@auburn.edu>
2019-11-22 14:54:24 -0600

    new implementation

--- Project3/code/LCS.py
+++ Project3/code/LCS.py
@@ -1,41 +1,57 @@
 from itertools import product
 
-def LCS(seq1, seq2, expected=None):
+class LCS:
+	def __init__(self, seq1, seq2):
+		# idiot check (should be prevented by fileIO)
+		assert(len(seq1) > 0 and len(seq2) > 0)
 
-	# idiot check (should be prevented by fileIO)
-	assert(len(seq1) > 0 and len(seq2) > 0)
+		# initialize the comparison matrix
+		_fill = [(0, '') for _ in range(len(seq2) + 1)] # get the vertical lists
+		self.matrix = [_fill for _ in range(len(seq1) + 1)] # arrange vertical lists
 
-	# initialize the comparison matrix
-	_fill = [(0, '') for _ in range(len(seq2) + 1)] # get the vertical lists
-	matrix = [_fill for _ in range(len(seq1) + 1)] # arrange vertical lists
+		self.seq1 = seq1
+		self.seq2 = seq2
 
-	"""
-	# initialize the solution matrix
-	_fill = ['' for _ in range(len(seq2) + 1)]
-	solution = [_fill for _ in range(len(seq1) + 1)]
-	"""
-
-	max_len = 0
-
-	# tuple of comp_matrix dimensions
-	dims = (range(1, len(seq1) + 1), range(1, len(seq2) + 1))
-
-	# nested loop but cleaner
-	for i,j in product(*dims):
-		if (seq1[i-1] == seq2[j-1]):
-			matrix[i][j] = (matrix[i-1][j-1] + 1, 'diag')
-			
-
-		else:
-			temp_a = matrix[i-1][j]
-			temp_b = matrix[i][j-1]
-
-			matrix[i][j] = (temp_a, 'up') if temp_a >= temp_b else (temp_b, 'left')
-
-			matrix[i][j] = max(matrix[i-1][j], matrix[i][j-1])
+		LCS()
+		self.LCS = build_seq()
 
 
+	def LCS(self):
+		# get loop dimensions
+		rng_1 = range(len(self.seq1), -1, -1)
+		rng_2 = range(len(self.seq2), -1, -1)
+		dims = (rng_1, rng_2)
 
+		# fill matrix (this is just an abstracted nested loop)
+		for i,j in product(*dims):
+			# if sequences match: 1 + diag value
+			if self.seq1[i] == self.seq2[j]:
+				self.matrix[i][j] = (1 + matrix[i+1][j+1], diag)
+			else:
+				tmp_a = (self.matrix[i+1][j], 'right')
+				tmp_b = (self.matrix[i][j+1], 'down')
 
+				# pick bottom value if >= right-value; else down-value
+				self.matrix[i][j] = tmp_a if tmp_a >= else tmp_b
 
+	def build_seq(self):
+		sequence = ''
 
+		# let m be length of sequence 1
+		# and n be length of sequence 2
+		m = len(self.seq1)
+		n = len(self.seq2)
+
+		i,j = 0,0
+		while(i < m and j < n):
+			if self.seq1[i] == self.seq2[j]:
+				sequence += self.seq1[i]
+				i += 1
+				j += 1
+
+			elif self.matrix[i+1][j][0] >= self.matrix[i][j+1][0];
+				i += 1
+			else:
+				j += 1
+
+		return sequence
---
+++ Project3/code/main.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.path.append('./code/')
+
+import LCS
+import fileIO
+
+if __name__ == '__main__':
+	
