c7249d385c2dbfa38d54a0ae88187d0191063f4c
Will McElhenney <whm0004@auburn.edu>
2019-11-22 15:04:18 -0600

    main written

--- Project3/code/main.py
+++ Project3/code/main.py
@@ -1,3 +1,4 @@
+from itertools import product
 import sys
 
 sys.path.append('./code/')
@@ -6,4 +7,21 @@ import LCS
 import fileIO
 
 if __name__ == '__main__':
-	
+	# error check input arguments
+	if len(sys.argv) >  2:
+		print("Too few arguments; need an input path and an output path")
+		quit()
+	elif len(sys.argv) < 4:
+		print("Too many arguments; just need an input path and an output path")
+		quit()
+
+	input_path = sys.argv[1]
+	output_path = sys.argv[2]
+
+	sequences = fileIO.read_input(input_path)
+
+	# find the LCS
+	for seq1, seq2 in product(sequences):
+		lcs = LCS(seq1, seq2)
+
+		fileIO.write_output(lcs.LCS)
